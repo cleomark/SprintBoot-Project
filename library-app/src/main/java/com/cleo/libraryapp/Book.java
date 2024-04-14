0f9188fbfb58637ece9c4dff2fc8f6690d0a9250
cleomark <cleocalitisin@gmail.com>
2024-04-14 16:20:43 +0800

    create book class

---
+++ library-app/src/main/java/com/cleo/libraryapp/Book.java
@@ -0,0 +1,45 @@
+package com.cleo.libraryapp;
+
+public class Book {
+    private String title;
+    private String author;
+    private int publicationYear;
+    private boolean available;
+
+    // Constructor
+    public Book(String title, String author, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.publicationYear = publicationYear;
+        this.available = true;
+    }
+
+    // Getter and Setter methods
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public void setAvailable(boolean available) {
+        this.available = available;
+    }
+
+    // Other methods
+    public void displayBookInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Publication Year: " + publicationYear);
+        System.out.println("Available: " + (available ? "Yes" : "No"));
+    }
+}
